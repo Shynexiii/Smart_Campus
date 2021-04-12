b7c1540a6d915f2f01c39dfd2086e91809132728
Aminou Inoussa Habib <habibaminou2@gmail.com>
2021-04-12 08:19:42 +0100

    Add Home Page in Web App

--- ChercheurProfile/App_Start/RouteConfig.cs
+++ ChercheurProfile/App_Start/RouteConfig.cs
@@ -12,7 +12,11 @@ namespace ChercheurProfile
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                "Home",
+                "",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
